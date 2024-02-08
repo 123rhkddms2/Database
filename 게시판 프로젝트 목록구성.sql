64f36f009ed5b43fc56639f4cdf8971bb644ceda
123rhkddms2 <123rhkddms2@gmail.com>
2024-02-08 12:44:06 +0900

    sql구문 추가

--- 게시판 프로젝트 목록구성.sql
+++ 게시판 프로젝트 목록구성.sql
@@ -1,18 +1,24 @@
-SELECT 
-	a.*,
-	b.`nick`
-FROM `Article` AS a
-JOIN `User` AS b ON a.writer = b.uid
-ORDER BY `no` DESC
-LIMIT 0, 10;
+
 
 INSERT INTO `Article` (`title`, `content`, `writer`, `regip`, `rdate`)
 SELECT `title`, `content`, `writer`, `regip`, `rdate` FROM `Article`;
 
-SELECT COUNT(*) FROM `Article`;
+
 
 SELECT * FROM `Article` WHERE `no`=1;
 
 DELETE FROM `Article` WHERE `no` > 5;
 
 UPDATE `Article` SET `hit`= `hit` + 1 WHERE `no`=1;
+
+SELECT 
+	a.*,
+	b.`nick`
+FROM `Article` AS a
+JOIN `User` AS b ON a.writer = b.uid
+WHERE `parent`=0
+ORDER BY `no` DESC
+LIMIT 0, 10;
+
+SELECT COUNT(*) FROM `Article` WHERE `parent`=0;
+
